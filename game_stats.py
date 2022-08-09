8319322206eb271113b70ad38ace17f4a9cde9f7
misaka16363 <a157772678@gmail.com>
2022-08-09 16:23:32 +0800

    first commit

---
+++ game_stats.py
@@ -0,0 +1,17 @@
+class GameStats:
+    """跟踪游戏的统计信息"""
+
+    def __init__(self, ai_game):
+        """初始化统计信息"""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        # 游戏刚启动时处于活动状态
+        self.game_active = True
+
+        # 让游戏一开始处于非活动状态
+        self.game_active = False
+
+    def reset_stats(self):
+        """初始化在游戏中运行期间了能变化的统计信息"""
+        self.ships_left = self.settings.ship_limit
