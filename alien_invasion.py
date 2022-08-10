aa6daa51c3b72bd0b500fb2cad3b6ce8be3601be
misaka16363 <a157772678@gmail.com>
2022-08-10 11:23:40 +0800

    渲染Play按钮 无法运行 位于分支 master 您的分支与上游分支 'origin/master' 一致。
    要提交的变更：
    	修改：     alien_invasion.py
    	修改：     button.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -34,6 +35,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        # 创建Play按钮
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         """开始游戏主循环"""
@@ -197,6 +201,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # 如果游戏处于非活动状态，就绘制Play按钮
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
